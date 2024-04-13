296036178e8528c98a1e54403d422710bf8b1f15
antoniobiasotti <abamorim36@gmail.com>
2024-04-13 16:07:47 -0300

    fix tailwindcss import + styled header

--- components/header.tsx
+++ components/header.tsx
@@ -2,13 +2,12 @@ import nlwUniteIcon from '../src/assets/nlw-unite-icon.svg'
 
 export function Header() {
     return (
-        <div className='flex items-center'>
-            <img src={nlwUniteIcon}/>
-            <nav>
-                <a href=''>Eventos</a>
-                <a href=''>Participantes</a>
+        <div className="flex items-center gap-5 py-2">
+            <img src={nlwUniteIcon} alt="NLW Unite Icon"/>
+            <nav className="flex items-center gap-5">
+                <a href="" className="font-medium text-sm">Eventos</a>
+                <a href="" className="font-medium text-sm">Participantes</a>
             </nav>
-
         </div>
     )
 }
--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { AttendeeList } from '../components/attendee-list';
 
 export function App() {
   return (
-    <div className='max-w-[1216px] mx-auto'>
-      <Header />
+    <div className="max-w-[1216px] mx-auto py-5">
+      <Header/>
       <AttendeeList />
     </div>
   )
