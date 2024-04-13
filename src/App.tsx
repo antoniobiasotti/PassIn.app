3e9fc2b53495bfb0c20969810af57822f5b8289b
antoniobiasotti <abamorim36@gmail.com>
2024-04-13 16:20:53 -0300

    add lucide lib + style List component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { AttendeeList } from '../components/attendee-list';
 
 export function App() {
   return (
-    <div className="max-w-[1216px] mx-auto py-5">
+    <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
       <Header/>
       <AttendeeList />
     </div>
