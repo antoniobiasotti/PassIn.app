c903af147e7686d57779d5f38d0fc58aa30ff418
antoniobiasotti <abamorim36@gmail.com>
2024-04-14 08:21:09 -0300

    add: nav-link component as <a> via extends and with spread operator

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,16 @@
 import nlwUniteIcon from '../src/assets/nlw-unite-icon.svg'
+import { NavLink } from './nav-link'
 
 export function Header() {
     return (
         <div className="flex items-center gap-5 py-2">
-            <img src={nlwUniteIcon} alt="NLW Unite Icon"/>
+            <img src={nlwUniteIcon} alt="NLW Unite Icon" />
+
             <nav className="flex items-center gap-5">
-                <a href="" className="font-medium text-sm">Eventos</a>
-                <a href="" className="font-medium text-sm text-zinc-300">Participantes</a>
+                <NavLink href="/eventos">Eventos</NavLink>
+                <NavLink href="/participants">Participants</NavLink>
             </nav>
         </div>
     )
 }
+
