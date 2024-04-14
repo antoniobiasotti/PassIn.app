3f43f1459c8ff20b603bde2a5a69c6b7407ea48d
antoniobiasotti <abamorim36@gmail.com>
2024-04-14 08:40:54 -0300

    add: icon-button component with conditional html attribute

--- components/attendee-list.tsx
+++ components/attendee-list.tsx
@@ -1,4 +1,5 @@
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, MoreHorizontal, Search } from 'lucide-react'
+import { IconButton } from '../components/icon-button';
 
 export function AttendeeList() {
     return(
@@ -43,9 +44,9 @@ export function AttendeeList() {
                                     <td className='py-3 px-4 text-sm text-zinc-300'>7 dias atrás</td>
                                     <td className='py-3 px-4 text-sm text-zinc-300'>3 dias atrás</td>
                                     <td className='py-3 px-4 text-sm text-zinc-300'>
-                                        <button className='bg-black/20 border-white/10 rounded-md p-1.5'>
+                                        <IconButton transparent>
                                             <MoreHorizontal className='size-4' />
-                                        </button>
+                                        </IconButton>
                                     </td>
                                 </tr>
                             )
@@ -61,18 +62,18 @@ export function AttendeeList() {
                                     <span>Página 1 de 23</span>
                                 
                                     <div className='flex gap-1.5'>
-                                        <button className='bg-white/20 border-white/10 rounded-md p-1.5'>
+                                        <IconButton>
                                             <ChevronsLeft className='size-4' />
-                                        </button>
-                                        <button className='bg-white/20 border-white/10 rounded-md p-1.5'>
+                                        </IconButton>
+                                        <IconButton>
                                             <ChevronLeft className='size-4' />
-                                        </button>
-                                        <button className='bg-white/20 border-white/10 rounded-md p-1.5'>
+                                        </IconButton>
+                                        <IconButton>
                                             <ChevronRight className='size-4' />
-                                        </button>
-                                        <button className='bg-white/20 border-white/10 rounded-md p-1.5'>
+                                        </IconButton>
+                                        <IconButton>
                                             <ChevronsRight className='size-4' />
-                                        </button>
+                                        </IconButton>
                                     </div>
                                 </div>
                             </td>
