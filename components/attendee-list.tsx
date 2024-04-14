2022e1cb430e4c699be59a19af885689b3a76349
antoniobiasotti <abamorim36@gmail.com>
2024-04-14 09:17:06 -0300

    add: tailwind merge to componente table-cell styling <td>

--- components/attendee-list.tsx
+++ components/attendee-list.tsx
@@ -2,6 +2,7 @@ import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, MoreHorizontal,
 import { IconButton } from '../components/icon-button';
 import { Table } from './table/table';
 import { TableHeader } from './table/table-header';
+import { TableCell } from './table/table-cell';
 
 export function AttendeeList() {
     return(
@@ -30,34 +31,34 @@ export function AttendeeList() {
                         {Array.from({length: 8}).map((_, i) => {
                             return (
                                 <tr key={i} className='border-b border-white/10 hover:bg-white/5'>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'>
+                                    <TableCell>
                                         <input type='checkbox' className='size-4 bg-black/20 rounded-lg border border-white/10 checked:bg-orange-400'/>
-                                    </td>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'>
+                                    </TableCell>
+                                    <TableCell>
                                         1234
-                                    </td>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'>
+                                    </TableCell>
+                                    <TableCell>
                                         <div className='flex flex-col gap-1'>
                                             <span className='font-semibold text-white'>Antônio Biasotti</span>
                                             <span>github.com/antoniobiasotti</span>
                                         </div>
-                                    </td>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'>7 dias atrás</td>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'>3 dias atrás</td>
-                                    <td className='py-3 px-4 text-sm text-zinc-300'>
+                                    </TableCell>
+                                    <TableCell>7 dias atrás</TableCell>
+                                    <TableCell>3 dias atrás</TableCell>
+                                    <TableCell>
                                         <IconButton transparent>
                                             <MoreHorizontal className='size-4' />
                                         </IconButton>
-                                    </td>
+                                    </TableCell>
                                 </tr>
                             )
                         })}
                     </tbody>
                     <tfoot>
                         <tr>
-                            <td className='py-3 px-4 text-sm text-zinc-300' colSpan={3}>
+                            <TableCell colSpan={3}>
                                 Mostrando 228 itens
-                            </td>
+                            </TableCell>
                             <td className='py-3 px-4 text-sm text-zinc-300 text-right' colSpan={3}>
                                 <div className='inline-flex items-center gap-8'>
                                     <span>Página 1 de 23</span>
